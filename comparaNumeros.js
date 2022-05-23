f61751cf843aed6acab7513828b37054e138ebca
iamlucasgomes <lucas.devjs@gmail.com>
2022-05-23 02:31:37 -0300

    upload de arquivos para o github

--- comparaNumeros.js
+++ comparaNumeros.js
@@ -5,16 +5,16 @@ function comparaNumeros(num1, num2) {
     } else {
         var iguais = "diferentes";
     };
-    if (soma > 10) {
+    if (SOMA > 10) {
         var maior = "maior";
     } else {
         var maior = "menor";
     };
-    if (soma > 20) {
+    if (SOMA > 20) {
         var menor = "maior";
     } else {
         var menor = "menor";
     };
-    return`Os numeros ${num1} e ${num2} são ${iguais}, sua soma é ${soma}, que é ${maior} que 10 e ${menor} que 20.`;
+    return`Os numeros ${num1} e ${num2} são ${iguais}, sua soma é ${SOMA}, que é ${maior} que 10 e ${menor} que 20.`;
 }
-console.log(comparaNumeros(2,2));
+console.log(comparaNumeros(1,2));
